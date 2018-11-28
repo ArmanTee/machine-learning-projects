20e54fc8a1efbb40df2b00befedc6636ecb914ce
Arman Tadjrishi <arman.tadjrishi@googlemail.com>
2018-11-28 00:07:20 +0000

    28112018

--- finding_donors/finding_donors_testing.py
+++ finding_donors/finding_donors_testing.py
@@ -1,4 +1,6 @@
 # Import libraries necessary for this project
+import logging
+logging.basicConfig(filename='example.log',level=logging.INFO)
 import numpy as np
 import pandas as pd
 from time import time
@@ -154,3 +156,111 @@ fscore = fbetaScore(0.5,recall,precision)
 
 # Print the results
 print("Naive Predictor: [Accuracy score: {:.4f}, F-score: {:.4f}]".format(accuracy, fscore))
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score,fbeta_score
+import inspect
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+a=SVC()
+
+def train_predict(learner, sample_size, X_train, y_train, X_test, y_test):
+    '''
+    inputs:
+       - learner: the learning algorithm to be trained and predicted on
+       - sample_size: the size of samples (number) to be drawn from training set
+       - X_train: features training set
+       - y_train: income training set
+       - X_test: features testing set
+       - y_test: income testing set
+    '''
+
+    results = {}
+
+
+
+    # TODO: Fit the learner to the training data using slicing with 'sample_size' using .fit(training_features[:], training_labels[:])
+    start = time() # Get start time
+    learner =learner.fit(X_train[:sample_size],y_train[:sample_size])
+    end = time() # Get end time
+
+    # TODO: Calculate the training time
+    results['train_time'] = end-start
+
+    # TODO: Get the predictions on the test set(X_test),
+    #       then get predictions on the first 300 training samples(X_train) using .predict()
+    start = time() # Get start time
+    predictions_test = learner.predict(X_test)
+    predictions_train = learner.predict(X_train[:300])
+    end = time() # Get end time
+
+    # TODO: Calculate the total prediction time
+    results['pred_time'] = end-start
+
+    # TODO: Compute accuracy on the first 300 training samples which is y_train[:300]
+    results['acc_train'] = accuracy_score(y_train[:300], predictions_train)
+
+    # TODO: Compute accuracy on test set using accuracy_score()
+    results['acc_test'] = accuracy_score(y_test, predictions_test)
+
+    # TODO: Compute F-score on the the first 300 training samples using fbeta_score()
+    results['f_train'] =  fbeta_score(y_train[:300], predictions_train,beta=0.5)
+
+    # TODO: Compute F-score on the test set which is y_test
+    results['f_test'] = fbeta_score(y_test, predictions_test,beta=0.5)
+
+    # Success
+    print("{} trained on {} samples.".format(learner.__class__.__name__, sample_size))
+
+    # Return the results
+    return results
+
+
+# TODO: Initialize the three models
+clf_A = LogisticRegression()
+clf_B = SVC(kernel="sigmoid")
+clf_C = DecisionTreeClassifier()
+
+# TODO: Calculate the number of samples for 1%, 10%, and 100% of the training data
+# HINT: samples_100 is the entire training set i.e. len(y_train)
+# HINT: samples_10 is 10% of samples_100 (ensure to set the count of the values to be `int` and not `float`)
+# HINT: samples_1 is 1% of samples_100 (ensure to set the count of the values to be `int` and not `float`)
+samples_100 = len(X_train)
+samples_10 = round(len(X_train)/10)
+samples_1 = round(len(X_train)/100)
+
+# Collect results on the learners
+results = {}
+for clf in [clf_A, clf_B, clf_C]:
+    clf_name = clf.__class__.__name__
+    results[clf_name] = {}
+    for i, samples in enumerate([samples_1, samples_10, samples_100]):
+        results[clf_name][i] = \
+        train_predict(clf, samples, X_train, y_train, X_test, y_test)
+
+# Run metrics visualization for the three supervised learning models chosen
+vs.evaluate(results, accuracy, fscore)
+from sklearn.grid_search import GridSearchCV
+from sklearn.metrics import make_scorer
+clf = LogisticRegression(random_state=20)
+make_scorer
+# HINT: parameters = {'parameter_1': [value1, value2], 'parameter_2': [value1, value2]}
+parameters = {'C':[0.2,0.3,0.31,0.32,0.33,0.34,0.35,0.36,0.37,0.38,0.39,0.4], 'penalty':['l1','l2']}
+scorer = make_scorer(fbeta_score,beta=0.5)
+
+# TODO: Perform grid search on the classifier using 'scorer' as the scoring method.
+grid_obj = GridSearchCV(clf, parameters, scoring=scorer)
+# TODO: Fit the grid search object to the training data and find the optimal parameters.
+grid_fit = grid_obj.fit(X_train, y_train)
+
+best_clf = grid_fit.best_estimator_
+predictions = (clf.fit(X_train, y_train)).predict(X_test)
+best_predictions = best_clf.predict(X_test)
+
+# Report the before-and-afterscores
+print("Unoptimized model\n------")
+print("Accuracy score on testing data: {:.4f}".format(accuracy_score(y_test, predictions)))
+print("F-score on testing data: {:.4f}".format(fbeta_score(y_test, predictions, beta = 0.5)))
+print("\nOptimized Model\n------")
+print("Final accuracy score on the testing data: {:.4f}".format(accuracy_score(y_test, best_predictions)))
+print("Final F-score on the testing data: {:.4f}".format(fbeta_score(y_test, best_predictions, beta = 0.5)))
